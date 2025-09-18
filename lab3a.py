f2b8bcbc2a8b6e5bb7550f08170d270537f1c8ee
Vaidehi Patel <vpatel278@myseneca.ca>
2025-09-18 01:59:37 -0400

    Added Lab 3 a

---
+++ lab3a.py
@@ -0,0 +1,39 @@
+#!/usr/bin/env python3
+
+# Author: Vaidehi Patel
+# Author ID: 166249219
+# Date Created: 2025/09/18
+# Course: OPS 445 NDD
+
+# return_text_value() function
+
+def return_text_value():
+    name = 'Terry'
+    greeting = 'Good Morning ' + name
+    return greeting
+
+# return_number_value() function
+
+def return_number_value():
+    num1 = 10
+    num2 = 5
+    num3 = num1 + num2
+    return num3
+
+
+
+
+# Main program
+
+
+
+
+
+if __name__ == '__main__':
+    print('python code')
+    text = return_text_value()
+    print(text)
+    number = return_number_value()
+    print(str(number))
+
+
